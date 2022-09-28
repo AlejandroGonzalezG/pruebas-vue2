4508f1d1136787847ef440484d14e26efcea1d67
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-09-28 18:15:44 -0300

    probando vuetify

--- prueba-vue/src/App.vue
+++ prueba-vue/src/App.vue
@@ -8,6 +8,9 @@
   </div>
 </template>
 
+
+
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- prueba-vue/src/views/HomeView.vue
+++ prueba-vue/src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Carrousel>
+    </Carrousel>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import Carrousel from '@/components/navbar.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    Carrousel
   }
 }
 </script>
